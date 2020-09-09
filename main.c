609a5d18f58a2411d7ca68cfaa8e9cbd3154b0c2
Ben Cravens <ben.conor.reese.cravens@gmail.com>
2020-09-09 15:07:23 +1200

    spellcheck seems to work

--- main.c
+++ main.c
@@ -14,26 +14,45 @@ static void print_info(int freq, char* word) {
 
 int main(int argc, char* argv[]) {
     /*declaring constants*/
-    const char *optstring = "ab:c";
+    const char *optstring = "c:df:orh";
+    extern char* optarg;
+    char* filename;
     char option;
-    FILE* ptr;
-    tree b = tree_new(RBT);
+    /*file pointer for graph output*/
+    FILE* graph;
+    /*file pointer for .txt file to be spellchecked*/
+    FILE* target;
+    tree dict = tree_new(RBT);
     char word[256];
-    ptr = fopen("tree-view.dot","w");
-    
+    graph = fopen("tree-view.dot","w");
+       
+ 
     /*reading in command line arguments*/
     while ((option = getopt(argc, argv, optstring)) != EOF) {
         switch (option) {
-            case 'a':
-                printf("it's A.\n");
-                break;
-            case 'b':
-                /*b's argument is available in the global
-                * variable optarg */
-                printf("it's B, argument %s\n",optarg);
-                break;
             case 'c':
-                printf("it's C.\n");
+                /*c's argument is available in the global
+                * variable optarg */
+                filename = optarg;
+                break;
+            case 'd':
+                /* this option should print the depth of the 
+                * tree to stdout and not do anything else */
+                break;
+            case 'f':
+                /*write the dot output to filename instead
+                * of the default filename if -o is also given.
+                * filename is stored in the global string optarg */
+                break;
+            case 'o':
+                /*output a representation of the tree in dot form to the file
+                * tree-view.dot using the functions given in output-dot.txt */
+                break;
+            case 'r':
+                /*make the tree an rbt instead of default bst*/
+                break;
+            case 'h':
+                /* print a help message describing how to use the program */
                 break;
             default:
                 printf("invalid command line argument");
@@ -41,20 +60,36 @@ int main(int argc, char* argv[]) {
         }
 
     }
- 
+
+    /*read words in from standard in, insert into our dictionary tree*/ 
     while(getword(word,256,stdin) != EOF) {
-        b = tree_insert(b,word);
+        dict = tree_insert(dict,word);
     }
 
-    printf("\n\n\n\n\n-----------------------\n\n\n\n");
-
-    tree_preorder(b,print_info);
-    if(b == NULL) {
-        printf("This shit null");
+    /*traverse tree*/
+    printf("printing dictionary. \n");
+    tree_preorder(dict,print_info);
+    printf("\n");
+    if(dict == NULL) {
+        printf("Empty dictionary..");
     }
-    printf("Tree depth: %d\n", tree_depth(b));
-    tree_output_dot(b, ptr);
-    tree_free(b);
-    fclose(ptr);
+
+    /*open file to be spellchecked*/
+    if (NULL == (target = fopen(filename, "r"))) {
+        fprintf(stderr, "ERROR: can't find file %s\n", filename);
+        return EXIT_FAILURE;
+    }
+    /*now read words in and spellcheck them*/
+    printf("printing misspelled words...\n");
+    while (1 == fscanf(target,"%s",word)) {
+        if (tree_search(dict,word) == 0) {
+            fprintf(stderr,"%s\n",word);
+        }
+    } 
+    /*output graph, free memory*/
+    tree_output_dot(dict, graph);
+    tree_free(dict);
+    fclose(graph);
+    fclose(target);
     return EXIT_SUCCESS;
 }
--- makefile
+++ makefile
@@ -1,6 +1,6 @@
 all:
 	gcc -W -Wall -O2 -ansi -pedantic -g tree.c mylib.c main.c -o output
-	./output -b "test input"
+	./output < dictionary.txt -c "test.txt"
 dot: 
 	dot -Tpdf < tree-view.dot > tree-view.pdf
 	okular tree-view.pdf
