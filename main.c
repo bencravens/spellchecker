9783183c675350de71766cbd0d3ef2dd8cd79829
Ben Cravens <ben.conor.reese.cravens@gmail.com>
2020-09-10 14:02:54 +1200

    figured out how to resolve multiple command line arguments. some are compatible, some are exclusive

--- main.c
+++ main.c
@@ -40,12 +40,44 @@ int main(int argc, char* argv[]) {
     int depth;
     char word[256];
     tree dict = NULL;
-    double fill_time;
-    double search_time;
+    double fill_time=0;
+    double search_time=0;
     clock_t tic, toc;
     int unknown_words = 0;
-    
-    /*reading in command line arguments*/
+    /*keep track of which cases we have had (as sometimes when we pass one command line arg others should be ignored)*/
+    int case_r = 0;
+    int case_c = 0;
+    int case_f = 0;
+    int case_o = 0;
+    /*reading in command line arguments, keeping track of which ones we have been passed*/
+    while ((option = getopt(argc, argv, optstring)) != EOF) {
+        switch (option) {
+            case 'r':
+                case_r = 1;
+                break;
+            case 'c':
+                case_c = 1;
+                break; 
+            case 'd':
+                break;
+           case 'o':
+                case_o = 1;
+                break;
+           case 'f':
+                case_f = 1;
+                break;
+           case 'h':
+                break;
+            default:
+                printf("invalid command line argument");
+                break;
+        }
+    }
+    /*optind is the argument of the option array we are at. need to reset it to iterate over command line args again*/
+    /* we iterate over the command line args to execute the actions they specify, now that we know all the args that 
+    * we have been passed (some args are mutually exclusive, while others are fine to combine*/
+    optind = 0;
+    /*now executing*/
     while ((option = getopt(argc, argv, optstring)) != EOF) {
         switch (option) {
             case 'r':
@@ -66,7 +98,7 @@ int main(int argc, char* argv[]) {
                 filename = optarg;
                 /* if -r has been specified, we will have a tree already..
                 * otherwise, we will make a tree... */
-                if (dict == NULL) {
+                if (case_r==0) {
                     /*declare tree as bst*/
                     dict = tree_new(BST); 
                     /*time insertion*/
@@ -101,7 +133,11 @@ int main(int argc, char* argv[]) {
                 /* this option should print the depth of the 
                 * tree to stdout and not do anything else */
                 /*make our tree if we haven't already*/
-                if (dict==NULL) {
+                /*if we have already done case_c, ignore this arg*/
+                if (case_c) {
+                    break;
+                }
+                if (case_r==0) {
                     /*declare tree as bst*/
                     dict = tree_new(BST); 
                     /*read words in from standard in, insert into our dictionary tree*/ 
@@ -114,22 +150,33 @@ int main(int argc, char* argv[]) {
                 /*write the dot output to filename instead
                 * of the default filename if -o is also given.
                 * filename is stored in the global string optarg */
-                graph = fopen(optarg,"w");
-                if (dict==NULL) {
+                /*we should only do this option if case o has also been passed, and there is no case c...*/
+                if (case_o==0 || case_c==1) {
+                    break;
+                }
+                /* just in case there isn't already a tree ...*/
+                if (case_r==0) {
                     /*declare tree as bst*/
                     dict = tree_new(BST); 
                     /*read words in from standard in, insert into our dictionary tree*/ 
-                    dict = writedict(dict, word);                 
+                    dict = writedict(dict, word);
                 }
-                tree_output_dot(dict,graph); 
+                graph = fopen(optarg,"w");
+                tree_output_dot(dict, graph);
                 fclose(graph);
                 break;
             case 'o':
                 /*output a representation of the tree in dot form to the file
                 * tree-view.dot using the functions given in output-dot.txt */
+                /*if we have already done case c or case f, we should break..*/
+                if (case_c) {
+                    break;
+                } else if (case_f) {
+                    break;
+                }
                 graph = fopen("tree-view.dot","w");
                 /* just in case there isn't already a tree ...*/
-                if (dict==NULL) {
+                if (case_r==0) {
                     /*declare tree as bst*/
                     dict = tree_new(BST); 
                     /*read words in from standard in, insert into our dictionary tree*/ 
@@ -138,7 +185,7 @@ int main(int argc, char* argv[]) {
                 tree_output_dot(dict, graph);
                 fclose(graph);
                 break;
-           case 'h':
+            case 'h':
                 /* print a help message describing how to use the program */
                 printf("this is a spellchecking program based on an implementation of a tree abstract data type, which supports both red black trees and binary search trees. \n");
                 printf("it accepts the following command line arguments:\n");
@@ -153,9 +200,7 @@ int main(int argc, char* argv[]) {
                 printf("invalid command line argument");
                 break;
         }
-
     }
-
     tree_free(dict);
     return EXIT_SUCCESS;
 }
--- makefile
+++ makefile
@@ -21,6 +21,13 @@ r:
 h:
 	gcc -W -Wall -O2 -ansi -pedantic -g tree.c mylib.c main.c -o output
 	./output < dictionary.txt -h
+check:
+	gcc -W -Wall -O2 -ansi -pedantic -g tree.c mylib.c main.c -o output
+	echo "testing c > f precedence. Should not output test.dot"
+	./output < smalldictionary.txt -r -c test.txt -o -f test.dot
+	ls | grep test
+	echo "testing f needs o, should not output test1.dot"
+	./output < smalldictionary.txt -r -f test1.dot
 memcheck:
 	gcc -W -Wall -O2 -ansi -pedantic -g tree.c mylib.c main.c -o output
 	valgrind ./output < smalldictionary.txt -r -c test.txt -f mydot.dot
@@ -28,3 +35,5 @@ memcheck:
 	okular tree-view.pdf
 	echo "now running sample program with same settings.."
 	./sample-asgn < smalldictionary.txt -r -c test.txt
+clean:
+	rm test.dot && rm test1.dot
